5bdffd919f03a9988460710a926fe32871607df0
Andreas Fröhlich <andreas@immerfroehlich.de>
2016-01-30 14:46:51 +0100

    Changed Midi Output Test

--- midi-test-sketches/MIDI_Output_Test/MIDI_Output_Test.ino
+++ midi-test-sketches/MIDI_Output_Test/MIDI_Output_Test.ino
@@ -7,15 +7,19 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 void setup()
 {
   pinMode(LED, OUTPUT);
-  MIDI.begin(MIDI_CHANNEL_OFF);
+  MIDI.begin();
 }
 
 void loop()
 {
-  digitalWrite(LED, HIGH);
-  MIDI.sendNoteOn(60, 127, 1);
-  delay(1000);		       
-  MIDI.sendNoteOff(60, 0, 1);
-  digitalWrite(LED, LOW);
-  delay(1000);
+
+  for(int i=50; i<60; i++) {
+    digitalWrite(LED, HIGH);
+    MIDI.sendNoteOn(i, 127, 1);
+    delay(1000);           
+    MIDI.sendNoteOff(i, 0, 1);
+    digitalWrite(LED, LOW);
+    delay(1000);
+  }
+  
 }
